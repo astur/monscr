6c42f5f93e266b2bbe6c5a78480b616224831745
astur <astur@yandex.ru>
2018-02-18 22:40:48 +0300

    indexes support

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-module.exports = db => async items => {
-    const _db = await db;
-    items = Array.isArray(items) ? items : [items];
-    items = items.map(item => {
-        const collection = item.errors ? 'errors' : 'data';
-        return _db.collection(collection).update(
-            {data: item.data},
-            {$set: item},
-            {upsert: true},
-        ).then(result => ({
-            modified: !!result.result.nModified,
-            upserted: !!result.result.upserted,
-            collection,
-        }));
-    });
-    return Promise.all(items);
+module.exports = db => {
+    db = (async () => {
+        const _db = await db;
+        const makeIndexes = v => _db.collection(v).createIndex({data: 1}, {unique: true});
+        await Promise.all(['data', 'errors'].map(makeIndexes));
+        return _db;
+    })();
+    return async items => {
+        const _db = await db;
+        items = Array.isArray(items) ? items : [items];
+        items = items.map(item => {
+            const collection = item.errors ? 'errors' : 'data';
+            return _db.collection(collection).update(
+                {data: item.data},
+                {$set: item},
+                {upsert: true},
+            ).then(result => ({
+                modified: !!result.result.nModified,
+                upserted: !!result.result.upserted,
+                collection,
+            }));
+        });
+        return Promise.all(items);
+    };
 };
