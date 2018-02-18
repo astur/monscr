c466ce628ed1c507963e2ec6e142f5769ed4e827
astur <astur@yandex.ru>
2018-02-18 22:31:44 +0300

    fake/todo test

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const test = require('ava');
 const monscr = require('.');
 
 test('monscr', t => {
-    t.true(true);
-    t.is(monscr, monscr);
+    t.is(typeof monscr, 'function');
 });
+
+test.todo('something with real mongo');
