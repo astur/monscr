152db96eba2b44bc0ce5308a1b277565e95a9dcf
astur <astur@yandex.ru>
2018-02-18 22:30:45 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = () => {};
+module.exports = db => async items => {
+    const _db = await db;
+    items = Array.isArray(items) ? items : [items];
+    items = items.map(item => {
+        const collection = item.errors ? 'errors' : 'data';
+        return _db.collection(collection).update(
+            {data: item.data},
+            {$set: item},
+            {upsert: true},
+        );
+    });
+    return Promise.all(items);
+};
