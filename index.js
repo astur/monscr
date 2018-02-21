b8284a1bcda1ba65a8dedfceefb62cd0dc68eb67
astur <astur@yandex.ru>
2018-02-21 19:19:02 +0300

    multi index support

--- index.js
+++ index.js
@@ -5,22 +5,32 @@ module.exports = (db, {
     cleanErrors = false,
     check = item => !('errors' in item),
 } = {}) => {
+    const arfy = v => Array.isArray(v) ? v : [null, undefined].includes(v) ? [] : [v];
+    index = arfy(index);
     db = (async () => {
         const _db = await db;
         if(cleanErrors){
             await _db.dropCollection(errors);
         }
-        const makeIndexes = v => _db.collection(v).createIndex({[index]: 1}, {unique: true});
+        const idx = index.reduce((r, v) => {
+            r[v] = 1;
+            return r;
+        }, {});
+        const makeIndexes = v => _db.collection(v).createIndex(idx, {unique: true});
         await Promise.all([valid, errors].map(makeIndexes));
         return _db;
     })();
     return async items => {
         const _db = await db;
-        items = Array.isArray(items) ? items : [null, undefined].includes(items) ? [] : [items];
+        items = arfy(items);
         items = items.map(item => {
             const collection = check(item) ? valid : errors;
+            const query = index.reduce((r, v) => {
+                r[v] = item[v];
+                return r;
+            }, {});
             return _db.collection(collection).update(
-                {[index]: item[index]},
+                query,
                 {$set: item},
                 {upsert: true},
             ).then(result => ({
